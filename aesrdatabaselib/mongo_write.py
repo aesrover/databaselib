74873a8fd499586dda15e7f44330cb2305974447
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-18 19:36:11 -0400

    Update MongoWrite with shorter variable and argument names

--- aesrdatabaselib/mongo_write.py
+++ aesrdatabaselib/mongo_write.py
@@ -2,9 +2,9 @@ import time
 from pymongo import MongoClient
 
 class MongoWrite:
-    def __init__(self, db, collection, *args, **kwargs):
+    def __init__(self, db, col, *args, **kwargs):
         # Create collection object
-        self.collection = ((MongoClient(*args, **kwargs))[db])[collection]
+        self.dbCol = ((MongoClient(*args, **kwargs))[db])[col]
         
         # Write saying start/initialisation time in epoch
         self.write({"atype":"START", "ver":"1.0", "ts":time.time()
@@ -12,4 +12,4 @@ class MongoWrite:
     
     def write(self, value):
         # write value to collection
-        return self.collection.insert(value)
+        return self.dbCol.insert(value)
