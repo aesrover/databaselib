96ad29308bcd75d07da2bf40448f78679a4fea37
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-07-10 18:51:30 -0400

    Make start message optional in mongo write

--- aesrdatabaselib/mongo_write.py
+++ aesrdatabaselib/mongo_write.py
@@ -2,13 +2,14 @@ import time
 from pymongo import MongoClient
 
 class MongoWrite:
-    def __init__(self, db, col, *args, **kwargs):
+    def __init__(self, db, col, startMessage= True, *args, **kwargs):
         # Create collection object
         self.dbCol = ((MongoClient(*args, **kwargs))[db])[col]
         
-        # Write saying start/initialisation time in epoch
-        self.write({"atype":"START", "ver":"1.0", "ts":time.time()
-                    , "comment":"Writing has started"})
+        if startMessage:
+            # Write saying start/initialisation time in epoch
+            self.write({"atype":"START", "ver":"1.0", "ts":time.time()
+                        , "comment":"Writing has started"})
     
     def write(self, value):
         # write value to collection
