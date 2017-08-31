4cea2f6b399fc14e417f3b866d47a3ae1b5f45e7
Lucien Gaitskell <lucieng@gaitskell.com>
2017-08-30 23:39:54 -0400

    Add 'MongoWaypointManager'

---
+++ aesrdatabaselib/mongo/waypoint.py
@@ -0,0 +1,26 @@
+from pymongo.collection import Collection
+
+from typing import List, Tuple
+
+from ..base.waypoint import WaypointManager
+
+
+class MongoWaypointManager(WaypointManager):
+    wp_filter = {'type': 'AUTO_WP'}
+
+    def __init__(self, wp_c: Collection):
+        super().__init__()
+        self.wp_c = wp_c
+
+    def get_wps(self, lim: int = None) -> List[Tuple[float, float]]:
+        return [e['pos'] for e in self.wp_c.find(self.wp_filter)]
+
+    def _get_del_wp(self):
+        wp = self.wp_c.find_one(self.wp_filter)
+        if wp is not None:
+            self.wp_c.delete_one(wp)
+            wp = wp['pos']
+        return wp
+
+    def remove_wp(self, wp):
+        self.wp_c.delete_one(wp)
