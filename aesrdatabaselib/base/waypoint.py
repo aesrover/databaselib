be5651538f473a6f667037e4a3a5292e2efb9393
Lucien Gaitskell <lucieng@gaitskell.com>
2017-08-30 23:39:12 -0400

    Add base 'WaypointManager'

---
+++ aesrdatabaselib/base/waypoint.py
@@ -0,0 +1,25 @@
+from typing import Tuple, List, Union
+
+
+class WaypointManager:
+    def __init__(self):
+        self.curr_wp = None
+
+    def __next__(self) -> Tuple[float, float]:
+        wp = self.select_next_wp()
+        if wp is None:
+            raise StopIteration
+        return wp
+
+    def select_next_wp(self) -> Union[None, Tuple[float, float]]:
+        self.curr_wp = self._get_del_wp()
+        return self.curr_wp
+
+    def get_wps(self, lim: int=None) -> List[Tuple[float, float]]:
+        raise NotImplementedError
+
+    def _get_del_wp(self):
+        raise NotImplementedError
+
+    def get_curr_wp(self):
+        return self.curr_wp
