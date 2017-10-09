e82bdcb94f03f481fe258267efb9d0dfcc0bd74d
Lucien Gaitskell <lucieng@gaitskell.com>
2017-10-09 09:10:22 -0400

    Use dict for wp return

--- aesrdatabaselib/mongo/waypoint.py
+++ aesrdatabaselib/mongo/waypoint.py
@@ -1,6 +1,6 @@
 from pymongo.collection import Collection
 
-from typing import List, Tuple
+from typing import List
 
 from ..base.waypoint import WaypointManager
 
@@ -12,14 +12,20 @@ class MongoWaypointManager(WaypointManager):
         super().__init__()
         self.wp_c = wp_c
 
-    def get_wps(self, lim: int = None) -> List[Tuple[float, float, float]]:
-        return [e['pos'] for e in self.wp_c.find(self.wp_filter)]
+    def __wp_as_dict(self, raw_wp):
+        return {
+            'pos': tuple(raw_wp['pos']),
+            'depth': raw_wp['depth']
+        }
+
+    def get_wps(self, lim: int = None) -> List[dict]:
+        return [self.__wp_as_dict(e) for e in self.wp_c.find(self.wp_filter)]
 
     def _get_del_wp(self):
         wp = self.wp_c.find_one(self.wp_filter)
         if wp is not None:
             self.wp_c.delete_one(wp)
-            wp = (*wp['pos'], wp['depth'])
+            wp = self.__wp_as_dict(wp)
         return wp
 
     def remove_wp(self, wp=None, pos=None, depth=None):
